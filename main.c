9781ecffab965dde7dc11086bbe58fb48b889b7a
Artemo Riperaldos <artemioarbaletos@gmail.com>
2016-03-05 11:50:18 +0300

    Cool Matrix App added

--- main.c
+++ main.c
@@ -10,31 +10,18 @@
 
 	SDL_Window* gWindow = NULL;
 	SDL_Renderer* gRenderer = NULL;
+	TTF_Font *gFont = NULL;
 
+int init();
+int load();
+int drawText(char* text, SDL_Color tColor, int x, int y);
 
 
 
 int main(int argc, char* args[])
-{	if (SDL_Init(SDL_INIT_VIDEO)<0)
-	{
-		printf("Kein init lolol! %s\n", SDL_GetError());
-	}
-	
-	int imgFlags = IMG_INIT_PNG;
-	if (!(IMG_Init(imgFlags) &imgFlags))
-	{
-		printf("Image init error! %s\n", IMG_GetError());
-	}
-
-	if (TTF_Init() == -1)
-	{
-		printf("Kein Fonts ahahaha %s\n", TTF_GetError());
-	}
-
-	gWindow = SDL_CreateWindow( "Forto Vorto", 0,0,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);	
-
-	gRenderer = SDL_CreateRenderer(gWindow, -1,SDL_RENDERER_ACCELERATED);
-
+{
+	if (!init()) close();
+	if (!load()) close();
 	SDL_SetRenderDrawColor(gRenderer, 0xBB, 0xBB,0xBB,0xBB);
 	
 	int quit = 0;
@@ -48,11 +35,14 @@ int main(int argc, char* args[])
 		}
 			
 		SDL_RenderClear(gRenderer);
+		drawText("ja Joba!",tColor,0,0);
 		SDL_RenderPresent(gRenderer);
 	}
+	TTF_CloseFont(gFont);
 	SDL_DestroyRenderer(gRenderer);
 	SDL_DestroyWindow(gWindow);
 
+	gFont = NULL;
 	gWindow = NULL;
 	gRenderer = NULL;
 
@@ -60,3 +50,74 @@ int main(int argc, char* args[])
 	IMG_Quit();
 	SDL_Quit();
 }
+
+int drawText(char* text, SDL_Color tColor, int x,int y)
+{
+	int ret = 1;
+	SDL_Surface* tSurface = TTF_RenderText_Solid(gFont, text, tColor);
+	SDL_Texture* tTexture = NULL;
+	if (!tSurface)
+	{
+		printf("kein loadung des text \"%s\", eraro %s!\n",text,SDL_GetError());
+		ret = 0;
+	}
+	
+	tTexture = SDL_CreateTextureFromSurface(gRenderer,tSurface);
+	if (!tTexture)
+	{
+		printf("Kein loading texture! %s\n",SDL_GetError());
+		ret = 0;
+	}
+
+	SDL_Rect tRect = {x,y,tSurface->w,tSurface->h};
+	SDL_RenderCopy(gRenderer,tTexture, NULL,&tRect);
+
+	SDL_FreeSurface(tSurface);
+	SDL_DestroyTexture(tTexture);
+}
+
+int init()
+{
+	int ret = 1;
+	if (SDL_Init(SDL_INIT_VIDEO)<0)
+	{
+		printf("Kein init lolol! %s\n", SDL_GetError());
+		ret = 0;
+	}
+	
+	int imgFlags = IMG_INIT_PNG;
+	if (!(IMG_Init(imgFlags) &imgFlags))
+	{
+		printf("Image init error! %s\n", IMG_GetError());
+		ret = 0;
+	}
+
+	if (TTF_Init() == -1)
+	{
+		printf("Kein Fonts ahahaha %s\n", TTF_GetError());
+		ret = 0;
+	}
+
+
+	gWindow = SDL_CreateWindow( "Forto Vorto", 0,0,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);	
+	if (!gWindow) ret = 0;
+
+	gRenderer = SDL_CreateRenderer(gWindow, -1,SDL_RENDERER_ACCELERATED);
+	if (!gRenderer) ret = 0;
+	return ret;
+}
+
+int load()
+{
+	int ret = 1;
+	gFont = TTF_OpenFont("res/COURIER.TTF",16);
+	if (!gFont)
+	{
+		printf("Some Shit with your Courier Font! %s\n",TTF_GetError());
+		ret = 0;
+	}
+	return ret;
+}
+
+
+
